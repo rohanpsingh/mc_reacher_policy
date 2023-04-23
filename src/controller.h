ab1bfe491261b7f60859591c79e5337fc55e8c86
Rohan P. Singh <rohan565singh@gmail.com>
2023-04-23 18:29:57 +0900

    create thread in controller

--- src/controller.h
+++ src/controller.h
@@ -3,12 +3,15 @@
 #include <mc_control/mc_controller.h>
 #include <mc_control/fsm/Controller.h>
 #include <mc_tasks/lipm_stabilizer/StabilizerTask.h>
+#include <condition_variable>
+#include <thread>
 
 #include "api.h"
 
 struct HandReacher_DLLAPI HandReacher : public mc_control::fsm::Controller
 {
     HandReacher(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
+    ~HandReacher();
 
     mc_rbdyn::Robot & controlRobot()
     {
@@ -21,6 +24,16 @@ struct HandReacher_DLLAPI HandReacher : public mc_control::fsm::Controller
 
 private:
     mc_rtc::Configuration config_;
+
+    std::map<std::string, double> msg;
+    std::string datastoreName_ = "PolicyPredictions";
+
+    // thread NN model execution
+    std::thread policy_th_;
+    bool policy_th_exit_ = false;
+    std::function<void()> * policy_work_ = nullptr;
+    std::condition_variable cv;
+    std::mutex policy_mutex_;
 
     std::shared_ptr<mc_tasks::lipm_stabilizer::StabilizerTask> stabilizer_;
     mc_rbdyn::lipm_stabilizer::StabilizerConfiguration
